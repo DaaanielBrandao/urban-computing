93d6877032a0bd000716c871015bb85ebb1dbf08
Daniel <38920805+DaaanielBrandao@users.noreply.github.com>
2022-01-03 19:33:23 +0000

    seasons/totd

--- 2_MGMPFM/lib/PoissonFactorModel.py
+++ 2_MGMPFM/lib/PoissonFactorModel.py
@@ -35,20 +35,56 @@ class PoissonFactorModel(object):
         file1 = open('./new_data/fsq_pois_cats.txt', 'r')
         Lines = file1.readlines()
         cat_dict = {}
-        self.T1 = {}
-        self.T0 = random.uniform(0, 0.5)
+        self.C1 = {}
+        self.C0 = random.uniform(0, 0.5)
         for line in Lines:
             line_split = line.split()
             cat_dict[int(line_split[0])] = line_split[1]
-            self.T1[line_split[1]] = random.uniform(0, 0.5)
-        #print(len(self.T1))
+            self.C1[line_split[1]] = random.uniform(0, 0.5)
+        #print(len(self.C1))
         #print(cat_dict)
         return cat_dict
+    def read_totd(self):
+        file1 = open('./new_data/fsq_poi_totd.txt', 'r')
+        Lines1 = file1.readlines()
+        file2 = open('./new_data/fsq_user_totd.txt', 'r')
+        Lines2 = file2.readlines()
+        totd_poi_dict = {}
+        totd_user_dict = {}
+        self.T1 = {}
+        self.T0 = random.uniform(0, 0.5)
+        for line in Lines1:
+            line_split = line.split()
+            totd_poi_dict[int(line_split[0])] = line_split[1]
+            self.T1[line_split[1]] = random.uniform(0, 0.5)
+        for line in Lines2:
+            line_split = line.split()
+            totd_user_dict[int(line_split[0])] = line_split[1]
+            self.T1[line_split[1]] = random.uniform(0, 0.5)
+        return totd_poi_dict, totd_user_dict
+    def read_season(self):
+        file1 = open('./new_data/fsq_poi_season.txt', 'r')
+        Lines1 = file1.readlines()
+        file2 = open('./new_data/fsq_user_season.txt', 'r')
+        Lines2 = file2.readlines()
+        season_poi_dict = {}
+        season_user_dict = {}
+        self.S1 = {}
+        self.S0 = random.uniform(0, 0.5)
+        for line in Lines1:
+            line_split = line.split()
+            season_poi_dict[int(line_split[0])] = line_split[1]
+            self.S1[line_split[1]] = random.uniform(0, 0.5)
+        for line in Lines2:
+            line_split = line.split()
+            season_user_dict[int(line_split[0])] = line_split[1]
+            self.S1[line_split[1]] = random.uniform(0, 0.5)
+        return season_poi_dict, season_user_dict
 
-    def Dist(self,T1,T0):
-        #print(T1,T0)
-        #print(math.sqrt((T1-T0)**2))
-        return  math.sqrt((T1-T0)**2)
+    def Dist(self,C1,C0):
+        #print(C1,C0)
+        #print(math.sqrt((C1-C0)**2))
+        return  math.sqrt((C1-C0)**2)
     def train(self, sparse_check_in_matrix, max_iters=50, learning_rate=1e-4):
         ctime = time.time()
         print("Training PFM...", )
@@ -68,6 +104,8 @@ class PoissonFactorModel(object):
         self.b =F[np.nonzero(F)].mean()
 
         self.cat = self.read_cat()
+        self.totd_poi, self.totd_user = self.read_totd()
+        self.season_poi, self.season_user = self.read_season()
 
 
         #print(F)
@@ -80,11 +118,15 @@ class PoissonFactorModel(object):
         
         #
         #self.b = np.mean(F_dok[np.where(F_dok != 0)])
-        self.b = np.mean(F_dok[np.nonzero(F_dok)])
-        print(self.b)
+        #self.b = np.mean(F_dok[np.nonzero(F_dok)])
+        #print(self.b)
         #
+        C1 = self.C1
+        C0 = self.C0
         T1 = self.T1
         T0 = self.T0
+        S1 = self.S1
+        S0 = self.S0
 
 
         tau = 10
@@ -97,9 +139,15 @@ class PoissonFactorModel(object):
                 for i,j in entry_index:
                     prediction = self.predict(i, j)
                     e = (F_Y[i,j] - prediction)
-                    #print(prediction)
-                    T1[self.cat[j]] += learning_rate_k*(e*(U[i].dot(L[j])* ((T1[self.cat[j]]-T0)/self.Dist(T1[self.cat[j]],T0)) - alpha* T1[self.cat[j]]))
-                    T0 += learning_rate_k*(e*(U[i].dot(L[j])* ((T1[self.cat[j]]-T0)/self.Dist(T1[self.cat[j]],T0)) - alpha* T0)) 
+                    #categories
+                    C1[self.cat[j]] += learning_rate_k*(e*(U[i].dot(L[j])* ((C1[self.cat[j]]-C0)/self.Dist(C1[self.cat[j]],C0)) - alpha* C1[self.cat[j]]))
+                    C0 += learning_rate_k*(e*(U[i].dot(L[j])* ((C1[self.cat[j]]-C0)/self.Dist(C1[self.cat[j]],C0)) - alpha* C0))
+                    #totd
+                    T1[self.totd_poi[j]] += learning_rate_k*(e*(U[i].dot(L[j])* ((T1[self.totd_poi[j]]-T0)/self.Dist(T1[self.totd_poi[j]],T0)) - alpha* T1[self.totd_poi[j]]))
+                    T0 += learning_rate_k*(e*(U[i].dot(L[j])* ((T1[self.totd_poi[j]]-T0)/self.Dist(T1[self.totd_poi[j]],T0)) - alpha* T0))
+                    #season
+                    S1[self.season_poi[j]] += learning_rate_k*(e*(U[i].dot(L[j])* ((S1[self.season_poi[j]]-S0)/self.Dist(S1[self.season_poi[j]],S0)) - alpha* S1[self.season_poi[j]]))
+                    S0 += learning_rate_k*(e*(U[i].dot(L[j])* ((S1[self.season_poi[j]]-S0)/self.Dist(S1[self.season_poi[j]],S0)) - alpha* S0))  
             for i, j in entry_index:
                 #prediction = self.predict(i, j)
                 #e = (F_Y[i,j] - prediction)
@@ -114,7 +162,9 @@ class PoissonFactorModel(object):
             L += learning_rate_k * ((F_Y.T).dot(U) + (alpha - 1) / L - 1 / beta)
             #
             self.U, self.L = U, L
+            self.C0, self.C1 = C0, C1
             self.T0, self.T1 = T0, T1
+            self.S0, self.S1 = S0, S1
             #
             loss = 0.0
             for i, j in entry_index:
@@ -130,14 +180,14 @@ class PoissonFactorModel(object):
         print("Done. Elapsed time:", time.time() - ctime, "s")
         self.U, self.L = U, L
 
-    def Sim(self,cat):
-        #print(self.Dist(self.T1[cat],self.T0))
-        return 1 - self.Dist(self.T1[cat],self.T0)
+    def Sim(self,cat,totd_user,totd_poi,season_user,season_poi):
+        #print(self.Dist(self.C1[cat],self.C0))
+        return (1 - self.Dist(self.C1[cat],self.C0))*(1 - self.Dist(self.T1[totd_user],self.T1[totd_poi]))*(1 - self.Dist(self.S1[season_user],self.S1[season_poi]))
 
     def predict(self, uid, lid, sigmoid=False):
         if sigmoid:
             return 1.0 / (1 + math.exp(-self.U[uid].dot(self.L[lid])))
         elif self.sim:
            #print(lid,lid in self.cat)
-            return self.U[uid].dot(self.L[lid]) * self.Sim(self.cat[lid]) #+ self.b_u[uid] + self.b_i[lid] + self.b
+            return self.U[uid].dot(self.L[lid]) * self.Sim(self.cat[lid], self.totd_user[uid],self.totd_poi[lid],self.season_user[uid],self.season_poi[lid]) #+ self.b_u[uid] + self.b_i[lid] + self.b
         return self.U[uid].dot(self.L[lid]) 
